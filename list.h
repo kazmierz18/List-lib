b124d87616f971df8cadaa12261ba88abe840a08
lukasz <kazmierz18@gmail.com>
2016-07-23 13:04:01 +0200

    Add  double linked list declaration and function to get choosen element from the list

--- list.h
+++ list.h
@@ -12,21 +12,36 @@
 extern "C" {
 #endif
     /*
-     * macro for initialization of a list
+     * macro for initialization of a linked list
      */
 #define List_init(ll) ll.next=NULL
+    /*
+     * macro for initialization ofa  double linked list
+     */
+#define List_double_init(ll) do{ll.next=NULL;ll.previous=NULL;}while(0)
     /*
      * Data type definition
      */
     typedef void* Data;
     
-    /*Structure holding pointers to Data and next element in the list
-     *if its last element of list next equals NULL
+    /*
+     * Structure holding pointers to Data and next element in the list
+     * if its last element of list next equals NULL
      */
     typedef struct List{
         Data data;
         struct List *next;
     }List;
+    /*
+     * Structure holding pointers to Data and next and previous element in the list
+     * last element of list next equals NULL
+     * first element of the list equals NULL
+     */
+    typedef struct List_double{
+        Data data;
+        struct List *next;
+        struct List *previous;
+    }List_double;
     
     /*
      * Adds a new element at the end of the list
@@ -44,6 +59,17 @@ extern "C" {
      * Note: d is a type Data which is a void pointer, needs to be cast
      */
     Data List_pop(List* ptr);
+    
+    /*
+     * Gets element from a list
+     * @ptr: pointer to begining of a list
+     * @index: chosen element from the list
+     * @return: Data of the last element (see note)
+     * if index exceeds number of elements on the list function returns NULL
+     * Note: d is a type Data which is a void pointer, needs to be cast
+     */
+    Data List_get_element(List* ptr,int index);
+    
 #ifdef	__cplusplus
 }
 #endif
