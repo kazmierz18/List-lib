cf69ec0b6a942ac4f1e4665c361b96ddbbb9fad3
lukasz <kazmierz18@gmail.com>
2016-07-24 11:29:15 +0200

    Add double linked list functions definitions

--- List.c
+++ List.c
@@ -10,6 +10,16 @@
             ptr->next->next=NULL;
             return ptr->next;
     }
+    List_double* List_d_put(List_double* ptr, Data d){
+        while(ptr->next!=NULL){
+            ptr=ptr->next;
+        }
+        ptr->next=malloc(sizeof(List_double));
+        ptr->next->data=d;
+        ptr->next->next=NULL;
+        ptr->next->previous=ptr;
+        return ptr->next;
+    }
     Data List_pop(List* ptr){
         List *pptr;
         Data d;
@@ -20,6 +30,16 @@
         d=ptr->data;
         free(ptr);
         pptr->next=NULL;
+        return d;
+    }
+    Data List_d_pop(List_double* ptr){
+        Data d;
+        while(ptr->next!=NULL){    
+            ptr=ptr->next;
+        }
+        d=ptr->data;
+        ptr->previous->next=NULL;
+        free(ptr);
         return d;
     }
     Data List_get_element(List *ptr, int index){
